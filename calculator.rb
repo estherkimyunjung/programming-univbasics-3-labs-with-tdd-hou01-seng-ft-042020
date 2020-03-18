b03e215b1d6e5b13677431dd7a76d9cb6e6f6ffb
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-18 15:41:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,7 @@
 # Add your variables here
 first_number = 5
 second_number = 3
-sum = #{first_number} + #{second_number}
+def sum (first_number, second_number)
+  puts #{first_number} + #{second_number}
+end
   
