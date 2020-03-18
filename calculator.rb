60ff11f238a45a5327b9fad7f5d21475208e6bed
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-18 15:53:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,5 @@
 # Add your variables here
 first_number = 5
 second_number = 3
-def add(first_number, second_number)
-  sum = first_number + second_number
-  puts sum
-end
+sum = first_number + second_number
 
